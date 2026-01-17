078d38e4043af5563b4e10ccf74cceea1d052236
Viscooo <104546312+viscomf12@users.noreply.github.com>
2026-01-18 00:59:23 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,16 +10,11 @@ def root():
 
 @app.post("/compare-bom")
 async def compare_bom(file: UploadFile = File(...)):
-    content = await file.read()
     excel = pd.ExcelFile(io.BytesIO(content))
-
-    bom1 = pd.read_excel(excel, sheet_name="bom1")
-    bom2 = pd.read_excel(excel, sheet_name="bom2")
-    bom3 = pd.read_excel(excel, sheet_name="bom3")
+    sheets = excel.sheet_names
 
     return {
-        "bom1_rows": len(bom1),
-        "bom2_rows": len(bom2),
-        "bom3_rows": len(bom3)
+        "sheets": sheets
     }
+
 
